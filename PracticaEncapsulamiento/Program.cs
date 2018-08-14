a9916622f16ac7938ee9ef90e57ad2117dc5fa27
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 08:33:59 -0700

    Agregar archivos de proyecto.

---
+++ PracticaEncapsulamiento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos una instancia de contacto
+            Contacto contactoUno = new Contacto();
+
+            contactoUno.Nombre = "José García";
+            contactoUno.Direccion = "Veracruz S/N";
+            contactoUno.CorreoElectronico = "Josegarcia@correo.com";
+            contactoUno.TelefonoCasa = "1234567890";
+            contactoUno.TelefonoCelular = "9870";
+
+            Console.WriteLine("Nombre: " + contactoUno.Nombre);
+            Console.WriteLine("Direccion: " + contactoUno.Direccion);
+            Console.WriteLine("Correo: " + contactoUno.CorreoElectronico);
+            Console.WriteLine("Tel. Casa: " + contactoUno.TelefonoCasa);
+            Console.WriteLine("Tel. Celular: " + contactoUno.TelefonoCelular);
+
+            Console.ReadLine();
+           
+        }
+    }
+}
